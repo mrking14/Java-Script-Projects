ceb9bc4db715cef50c74fb48cb6c2bbeaecd15f1
Masoom Raza <masoomraza843113@outlook.com>
2024-03-28 09:35:23 +0530

    "Fixed JavaScript Error Code"

--- Stone Paper And ScissorGame/script.js
+++ Stone Paper And ScissorGame/script.js
@@ -1,13 +1,14 @@
 let userScore = 0;
 let compScore =0;
-const choices = document.querySelector(".choices");
+const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
 
+
 const genCompChoice=()=>{
     const option = ["rock","paper","scissor"];
-    const randIdx = Maht.floor(Math.random()*3);
+    const randIdx = Math.floor(Math.random()*3);
     return option[randIdx];
 };
 
@@ -50,6 +51,7 @@ const playGame=(userChoice)=>{
 };
 
 choices.forEach((choice) => {
+    console.log(choice)
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
     playGame(userChoice);
